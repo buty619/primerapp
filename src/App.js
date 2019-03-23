7bd14547d58c3c9ae796dedfb225470f8b9cd837
Cristian Felipe Buitrago Rodriguez <36422189+buty619@users.noreply.github.com>
2019-03-23 16:53:28 -0500

    push

--- src/App.js
+++ src/App.js
@@ -2,13 +2,24 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      number:0
+    }
+  }
   render() {
     return (
       <div>
-        <h1>Mi primera aplicación</h1>
-        <p>Esta es mi primera aplicación en React, está padrísimo!</p>
+        <h1>{this.state.number}</h1>
+        <button onClick={this.addOne.bind(this)}>Add One</button>
       </div>
     );
+  }
+  addOne(){
+    this.setState({      
+      number : this.state.number + 1
+    });
   }
 }
 
