d05cc37033d697f7473de9929d77cf135c767fc8
Cristian Felipe Buitrago Rodriguez <36422189+buty619@users.noreply.github.com>
2019-03-25 20:12:00 -0500

    p

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <h1>{this.state.number}</h1>
+        <h1 class="value">{this.state.number}</h1>
         <button onClick={this.addOne.bind(this)}>Incrementa</button>
       </div>
     );
